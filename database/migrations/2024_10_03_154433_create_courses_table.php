32e28c023aebaa754fb3cf9ba1409f5adb6f9928
mokadde <mokaddes.ru2000@gmail.com>
2024-10-03 22:05:34 +0600

    commit

--- database/migrations/2024_10_03_154433_create_courses_table.php
+++ database/migrations/2024_10_03_154433_create_courses_table.php
@@ -13,6 +13,19 @@ return new class extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 255);
+            $table->unsignedBigInteger('book')->default(0);
+            $table->unsignedSmallInteger('year');
+            $table->float('price')->default(0.00);
+            $table->string('image', 255)->nullable();
+            $table->float('price')->default(0);
+            $table->string('content', 255);
+            $table->string('link', 255);
+            $table->unsignedBigInteger('submitted_by');
+            $table->float('duration')->default(0.00);
+            $table->unsignedTinyInteger('level')->default(0);
+            $table->int('platform_id')->default(0);
+            $table->PRIMARY KEY (`id`)
             $table->timestamps();
         });
     }
